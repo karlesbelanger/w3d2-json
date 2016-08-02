f4a0425ac8f8d832b6af71fe565dfdc4bb823ac3
karlesbelanger <karlesbelanger@gmail.com>
2016-08-02 16:23:01 -0400

    Parse student done

--- app/src/main/java/ca/kgb/jsonparsing/MainActivity.java
+++ app/src/main/java/ca/kgb/jsonparsing/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
             "{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},"+
             "{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},"+
             "{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]";
+    private String jsonString2 = "{\"student\":[{\"name\":\"Juan\",\"age\":20,\"grade\":8.1},"+
+            "{\"name\":\"Miguel\",\"age\":23,\"grade\":8.3},"+
+            "{\"name\":\"Roberto\",\"age\":39,\"grade\":9.3},"+
+            "{\"name\":\"Luis\",\"age\":19,\"grade\":6.9},"+
+            "{\"name\":\"Gaudencio\",\"age\":25,\"grade\":4.3}]}";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +33,32 @@ public class MainActivity extends AppCompatActivity {
         try {
             JSONArray jsonArray = new JSONArray(jsonString);
             JSONObject jsonObject;
+            String name;
+            int age;
+            double grade;
+            for (int i = 0; i < jsonArray.length(); i++) {
+//                Log.d(TAG, "DoMagic 'i':" + i + " : "+ jsonArray.get(i));
+                jsonObject = new JSONObject(jsonArray.get(i).toString());
+                name = jsonObject.getString("name");
+                age = Integer.parseInt( jsonObject.getString("age"));
+                grade = Double.parseDouble( jsonObject.getString("grade"));
+                Log.d(TAG, "DoMagic 'i':" + i +
+                        ", Name : "+ name +
+                        ", Grade : " + grade +
+                        ", Age : " + age);
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+
+    }
+    public void DoMagic2(View view) {
+        try {
+
+            JSONArray jsonArray;
+            JSONObject jsonObject;
+            jsonObject = new JSONObject(jsonString2);
+            jsonArray = jsonObject.getJSONArray("student");
             String name;
             int age;
             double grade;
